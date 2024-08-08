1f5831365a91a8363c66ba070573fe58b2e77c1b
nilar <info@nilarholdings.com>
2024-08-08 23:31:50 +0630

    8july

--- test/src/App.js
+++ test/src/App.js
@@ -5,12 +5,14 @@ import User from "./components/users/User";
 // import Haynow from "./components/Haynow"
 
 function App() {
+  let image = "https://th.bing.com/th/id/OIP.Q50A1jwXSoOLSteU4QiVgwAAAA?w=256&h=256&rs=1&pid=ImgDetMain";
   return (
    <div> {
     // // [0,1,2,3,4,5,6,7,8].map( ele => <Haynow key={ele} />)
     // <Haynow />
     <div className="container my-5 ">
-          <User />
+          <User image={image} phone="0987654321" cell="0987878787" />
+          <User image={image} phone="0912345678" cell="0987654328" />
     </div>
 
 }
--- test/src/components/users/User.js
+++ test/src/components/users/User.js
@@ -1,15 +1,15 @@
 import React from "react"
 
-function User(){
+function User({image, phone , cell}){
     return (
     <div className="card">
         <div className="row">
             <div className="col-3">
-                <img src="https://th.bing.com/th/id/OIP.Q50A1jwXSoOLSteU4QiVgwAAAA?w=256&h=256&rs=1&pid=ImgDetMain" width="50px" height="50px" />
+                <img src={image} width="50px" height="50px" />
             </div>
             <div className="col-5">
-                <p>phone:09876543</p>
-                <p>cell:888999213</p>
+                <p>{phone}</p>
+                <p>{cell}</p>
             </div>
             <div className="col-4">
                 <button className="btn btn-danger btn-sm mt-4">
